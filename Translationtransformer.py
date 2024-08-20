a0ad796d5920ec9734a920e633375fd4cec980b0
Rishi Shah <smitjohn3221@gmail.com>
2024-08-20 00:07:16 -0400

    Add files via upload

--- Translationtransformer.py
+++ Translationtransformer.py
@@ -69,3 +69,30 @@ class Transformer(nn.Module):
         trg_sub_mask = torch.tril(torch.ones((trg_len, trg_len), device=self.device)).bool()
         trg_mask = trg_pad_mask & trg_sub_mask
         return trg_mask
+    
+# Implementing the Encoder
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, n_layers, n_heads, pf_dim, dropout, device, max_length):
+        super().__init__()
+        
+        self.device = device
+        self.tok_embedding = nn.Embedding(input_dim, hidden_dim)
+        self.pos_embedding = nn.Embedding(max_length, hidden_dim)
+        
+        self.layers = nn.ModuleList([EncoderLayer(hidden_dim, n_heads, pf_dim, dropout, device) for _ in range(n_layers)])
+        self.dropout = nn.Dropout(dropout)
+        
+        self.scale = torch.sqrt(torch.FloatTensor([hidden_dim])).to(device)
+        
+    def forward(self, src, src_mask):
+        batch_size = src.shape[0]
+        src_len = src.shape[1]
+        
+        pos = torch.arange(0, src_len).unsqueeze(0).repeat(batch_size, 1).to(self.device)
+        
+        src = self.dropout((self.tok_embedding(src) * self.scale) + self.pos_embedding(pos))
+        
+        for layer in self.layers:
+            src = layer(src, src_mask)
+            
+        return src
