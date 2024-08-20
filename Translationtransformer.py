6793eb5f19cddca149d35c7570d6856f930552a4
Rishi Shah <smitjohn3221@gmail.com>
2024-08-19 22:48:37 -0400

    Add files via upload

--- Translationtransformer.py
+++ Translationtransformer.py
@@ -33,3 +33,39 @@ train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
     (train_data, valid_data, test_data),
     batch_size=BATCH_SIZE,
     device=device)
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Transformer(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dim, n_layers, n_heads, pf_dim, dropout, device, max_length=100):
+        super().__init__()
+        
+        self.device = device
+        
+        self.encoder = Encoder(input_dim, hidden_dim, n_layers, n_heads, pf_dim, dropout, device, max_length)
+        self.decoder = Decoder(output_dim, hidden_dim, n_layers, n_heads, pf_dim, dropout, device, max_length)
+        self.src_tok_emb = nn.Embedding(input_dim, hidden_dim)
+        self.trg_tok_emb = nn.Embedding(output_dim, hidden_dim)
+        self.fc_out = nn.Linear(hidden_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, src, trg):
+        src_mask = self.make_src_mask(src)
+        trg_mask = self.make_trg_mask(trg)
+        
+        enc_src = self.encoder(src, src_mask)
+        output, attention = self.decoder(trg, enc_src, trg_mask, src_mask)
+        
+        return output, attention
+    
+    def make_src_mask(self, src):
+        src_mask = (src != SRC.vocab.stoi[SRC.pad_token]).unsqueeze(1).unsqueeze(2)
+        return src_mask
+    
+    def make_trg_mask(self, trg):
+        trg_pad_mask = (trg != TRG.vocab.stoi[TRG.pad_token]).unsqueeze(1).unsqueeze(2)
+        trg_len = trg.shape[1]
+        trg_sub_mask = torch.tril(torch.ones((trg_len, trg_len), device=self.device)).bool()
+        trg_mask = trg_pad_mask & trg_sub_mask
+        return trg_mask
