7faaff268a72725ec5129b5edb03368f97107850
Rishi Shah <smitjohn3221@gmail.com>
2024-08-20 01:25:31 -0400

    Add files via upload

--- Translationtransformer.py
+++ Translationtransformer.py
@@ -96,3 +96,21 @@ class Encoder(nn.Module):
             src = layer(src, src_mask)
             
         return src
+    
+# Implementing the Encoder Layer
+class EncoderLayer(nn.Module):
+    def __init__(self, hidden_dim, n_heads, pf_dim, dropout, device):
+        super().__init__()
+        
+        self.self_attn_layer_norm = nn.LayerNorm(hidden_dim)
+        self.ff_layer_norm = nn.LayerNorm(hidden_dim)
+        self.self_attention = MultiHeadAttentionLayer(hidden_dim, n_heads, dropout, device)
+        self.positionwise_feedforward = PositionwiseFeedforwardLayer(hidden_dim, pf_dim, dropout)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, src, src_mask):
+        _src, _ = self.self_attention(src, src, src, src_mask)
+        src = self.self_attn_layer_norm(src + self.dropout(_src))
+        _src = self.positionwise_feedforward(src)
+        src = self.ff_layer_norm(src + self.dropout(_src))
+        return src
